a2299dfaeca99e6d84159235c8a8fa671676cf08
Samuel Hipponen <43549635+samuelhip96@users.noreply.github.com>
2018-09-25 07:20:57 +0300

    Changed colors, camera perspective etc

--- demos/simple-rendering.js
+++ demos/simple-rendering.js
@@ -126,11 +126,11 @@ let triangles = new Uint16Array([
 let fragmentShader = `
     #version 300 es
     precision highp float;
-    
+
     in vec4 color;
-    
+
     out vec4 outColor;
-    
+
     void main()
     {
         outColor = color;
@@ -145,22 +145,22 @@ let fragmentShader = `
 // language=GLSL
 let vertexShader = `
     #version 300 es
-    
+
     uniform vec4 bgColor;
     uniform vec4 fgColor;
     uniform mat4 modelViewMatrix;
     uniform mat4 modelViewProjectionMatrix;
-    
+
     layout(location=0) in vec3 position;
     layout(location=1) in vec3 normal;
-    
+
     out vec4 color;
-    
+
     void main()
     {
-        gl_Position = modelViewProjectionMatrix * vec4(position, 1.0);
+        gl_Position = modelViewProjectionMatrix * vec4(position, 0.8);
         vec3 viewNormal = (modelViewMatrix * vec4(normal, 0.0)).xyz;
-        color = mix(bgColor * 0.8, fgColor, viewNormal.z) + pow(viewNormal.z, 10.0);
+        color = mix(bgColor * 0.6, fgColor, viewNormal.z) + pow(viewNormal.z, 10.0);
     }
 `;
 
@@ -169,13 +169,13 @@ let vertexShader = `
 // **             Application processing               **
 // ******************************************************
 
-let bgColor = vec4.fromValues(1.0, 0.2, 0.3, 1.0);
-let fgColor = vec4.fromValues(1.0, 0.9, 0.5, 1.0);
+let bgColor = vec4.fromValues(0.2, 0.8, 0.9, 0.5);
+let fgColor = vec4.fromValues(0.0, 0.0, 0.0, 0.5);
 
 
 app.clearColor(bgColor[0], bgColor[1], bgColor[2], bgColor[3])
     // .depthTest();
-    .cullBackfaces();
+    //.cullBackfaces();
 
 let program = app.createProgram(vertexShader.trim(), fragmentShader.trim());
 
@@ -204,12 +204,12 @@ let startTime = new Date().getTime() / 1000;
 function draw() {
     let time = new Date().getTime() / 1000 - startTime;
 
-    mat4.perspective(projMatrix, Math.PI / 4, app.width / app.height, 0.1, 100.0);
-    mat4.lookAt(viewMatrix, vec3.fromValues(3, 0, 2), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
+    mat4.perspective(projMatrix, Math.PI / 25, app.width / app.height, 0.9, 100.0);
+    mat4.lookAt(viewMatrix, vec3.fromValues(50, 25, 20), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
     mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
 
-    mat4.fromXRotation(rotateXMatrix, time * 0.1136);
-    mat4.fromYRotation(rotateYMatrix, time * 0.2235);
+    mat4.fromXRotation(rotateXMatrix, time * 0.0);
+    mat4.fromYRotation(rotateYMatrix, time * 0.3);
     mat4.multiply(modelMatrix, rotateXMatrix, rotateYMatrix);
 
     mat4.multiply(modelViewMatrix, viewMatrix, modelMatrix);
